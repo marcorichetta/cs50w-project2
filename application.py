8f366b9648a4bbe3e2d4c3ca3a9063531d422771
marcorichetta <marcorichetta@gmail.com>
2019-01-09 11:47:34 -0300

    Se guarda session en Flask para renderear la navbar.

--- application.py
+++ application.py
@@ -1,16 +1,28 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SECRET_KEY"] = "my secret key"
 socketio = SocketIO(app)
 
 @app.route("/")
 def index():
+
     return render_template("index.html")
 
 @app.route("/signin", methods=['GET','POST'])
 def signin():
+    ''' Save the username on a Flask session 
+    after the user submit the sign in form '''
+
+    if request.method == "POST":
+        username = request.form.get("username")
+
+        session["username"] = username
+
+        redirect("/")
+
+
     return render_template("signin.html")
