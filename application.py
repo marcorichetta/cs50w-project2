3c35b2656a931b47336df6ce28594fb3047d6d57
Marco Richetta <marcorichetta@gmail.com>
2019-01-15 14:39:54 -0300

    Done: User and channel creation, channels list and channel page.

--- application.py
+++ application.py
@@ -39,13 +39,36 @@ def signin():
     else:
         return render_template("signin.html")
 
-@app.route("/create", methods=['POST'])
+@app.route("/create", methods=['GET','POST'])
 def create():
+
+    # Get channel name from form
+    newChannel = request.form.get("channel")
 
     if request.method == "POST":
 
-        session["variable"] = request.form.get("channel")
+        if newChannel in channelsCreated:
+            return render_template("error.html", message="that channel already exists!")
+        
+        session["current_channel"] = newChannel
 
-        channelsCreated.append(request.form.get("channel"))
+        # Add channel to global list of channels
+        channelsCreated.append(newChannel)
 
-        return render_template("index.html", channels = channelsCreated)
+        return redirect("/channels/" + newChannel)
+        #return render_template("index.html", channels = channelsCreated)
+    
+    else:
+
+        return render_template("create.html", channels = channelsCreated)
+
+@app.route("/channels/<channel>", methods=['GET','POST'])
+@login_required
+def channel(channel):
+    """ Show channel page to send and receive messages """
+
+    if request.method == "POST":
+        
+        return redirect("/")
+    else:
+        return render_template("channel.html")
