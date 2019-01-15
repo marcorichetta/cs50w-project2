f1f83d7dd6d2c7b4a58301a902c1bf923f44134b
Marco Richetta <marcorichetta@gmail.com>
2019-01-15 12:29:25 -0300

    Cookie stored client side and login_required implemented.

--- application.py
+++ application.py
@@ -3,13 +3,17 @@ import os
 from flask import Flask, render_template, session, request, redirect
 from flask_socketio import SocketIO, emit
 
+from helpers import login_required
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "my secret key"
 socketio = SocketIO(app)
 
+# Keep track of channels created
 channelsCreated = []
 
 @app.route("/")
+@login_required
 def index():
 
     return render_template("index.html", channels=channelsCreated)
@@ -19,21 +23,28 @@ def signin():
     ''' Save the username on a Flask session 
     after the user submit the sign in form '''
 
+    # Forget any username
+    session.clear()
+
+    username = request.form.get("username")
+    
     if request.method == "POST":
-        username = request.form.get("username")
+        
+        session["username"] = username
 
-        session['username'] = username
+        # Remember the user session on a cookie if the browser is closed.
+        session.permanent = True
 
-        redirect("/")
-
-    return render_template("signin.html")
+        return redirect("/")
+    else:
+        return render_template("signin.html")
 
 @app.route("/create", methods=['POST'])
 def create():
 
     if request.method == "POST":
 
-        session['variable'] = request.form.get("channel")
+        session["variable"] = request.form.get("channel")
 
         channelsCreated.append(request.form.get("channel"))
 
