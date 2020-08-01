e67cd89da9bcfcb16c7e374e54107ce47851a08c
Marco Richetta <marcorichetta@gmail.com>
2020-08-01 19:19:06 -0300

    Updated requirements and removed GET from /create (Outdated)

--- application.py
+++ application.py
@@ -38,7 +38,7 @@ def signin():
     
     if request.method == "POST":
 
-        if len(username) < 1 or username is '':
+        if len(username) < 1 or username == '':
             return render_template("error.html", message="username can't be empty.")
 
         if username in usersLogged:
@@ -70,31 +70,25 @@ def logout():
 
     return redirect("/")
 
-@app.route("/create", methods=['GET','POST'])
+@app.route("/create", methods=['POST'])
 def create():
     """ Create a channel and redirect to its page """
 
     # Get channel name from form
     newChannel = request.form.get("channel")
 
-    if request.method == "POST":
-
-        if newChannel in channelsCreated:
-            return render_template("error.html", message="that channel already exists!")
-        
-        # Add channel to global list of channels
-        channelsCreated.append(newChannel)
-
-        # Add channel to global dict of channels with messages
-        # Every channel is a deque to use popleft() method 
-        # https://stackoverflow.com/questions/1024847/add-new-keys-to-a-dictionary
-        channelsMessages[newChannel] = deque()
-
-        return redirect("/channels/" + newChannel)
+    if newChannel in channelsCreated:
+        return render_template("error.html", message="that channel already exists!")
     
-    else:
+    # Add channel to global list of channels
+    channelsCreated.append(newChannel)
 
-        return render_template("create.html", channels = channelsCreated)
+    # Add channel to global dict of channels with messages
+    # Every channel is a deque to use popleft() method 
+    # https://stackoverflow.com/questions/1024847/add-new-keys-to-a-dictionary
+    channelsMessages[newChannel] = deque()
+
+    return redirect("/channels/" + newChannel)
 
 @app.route("/channels/<channel>", methods=['GET','POST'])
 @login_required
